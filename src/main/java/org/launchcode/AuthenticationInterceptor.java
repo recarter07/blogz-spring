37d03c95626d09ec5528c50531196b70190e18d4
Ruth Carter <recarter07@gmail.com>
2016-11-14 17:50:11 -0600

    first comment added to given code

--- src/main/java/org/launchcode/AuthenticationInterceptor.java
+++ src/main/java/org/launchcode/AuthenticationInterceptor.java
@@ -21,6 +21,7 @@ public class AuthenticationInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
 
+    	// restricted URLs
         List<String> authPages = Arrays.asList("/blog/newpost");
 
         // Require sign-in for auth pages
